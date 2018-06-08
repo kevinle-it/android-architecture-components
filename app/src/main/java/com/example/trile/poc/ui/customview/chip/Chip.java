ce0eccfdf1fe65ac053e4ed99bc59aa86bdef8ea
Tri Le <tri.le@notabasement.com>
2018-06-08 20:47:28 +0700

    Optimize Imports.

--- app/src/main/java/com/example/trile/poc/ui/customview/chip/Chip.java
+++ app/src/main/java/com/example/trile/poc/ui/customview/chip/Chip.java
@@ -11,7 +11,6 @@ import android.util.Log;
 import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
 
 import com.example.trile.poc.R;
 import com.example.trile.poc.utils.Objects;
